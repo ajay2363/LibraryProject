1c16f34417f2bb5d67fd4b83a0b0988ecb193ded
ajay326072@gmail.com <ajay326072@gmail.com>
2019-05-10 12:45:51 -0400

    Did 20 mins of video 3 - Not in sync yet

--- LibraryProject/Controllers/CatalogController.cs
+++ LibraryProject/Controllers/CatalogController.cs
@@ -39,5 +39,25 @@ namespace LibraryProject.Controllers
             return View(model);
                
         }
+        public IActionResult Detail (int id)
+        {
+            var asset = _assets.GetByID(id);
+
+            var model = new AssetDetailModel
+            {
+                AssetId = id,
+                Title = asset.Title,
+                Year = asset.Year,
+                Cost = asset.Cost,
+                Status = asset.Status.Name,
+                ImageUrl = asset.ImageUrl,
+                AuthororDirector = _assets.getAuthororDirector(id),
+                CurrentLocation = _assets.GetCurrentLocation(id).Name,
+                DeweyCallNumber = _assets.GetDeweyIndex(id),
+                ISBN = _assets.GetIsbn(id)
+
+            };
+            return View(model);
+        }
     }
 }
